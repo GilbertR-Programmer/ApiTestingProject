20dbc8236593a44fc7659c02ca13fc1546ac6242
Gilbert Reid <greid@spartaglobal.com>
2024-09-05 16:09:29 +0100

    removed verbose call of shareddata in generic step defs

--- PetStore3TestingFramework/src/test/java/stepdefs/GenericStepDefs.java
+++ PetStore3TestingFramework/src/test/java/stepdefs/GenericStepDefs.java
@@ -4,12 +4,13 @@ import io.cucumber.java.en.Then;
 import models.ApiModel;
 import org.junit.jupiter.api.Assertions;
 import utils.SharedData;
+import static utils.SharedData.*;
 
 public class GenericStepDefs {
 
     @Then("I should receive a {int} status code")
     public void iShouldReceiveASuccessfulOperationStatusCode(int expectedStatusCode) {
-        Assertions.assertEquals(expectedStatusCode, SharedData.getInstance().response.getStatusCode());
+        Assertions.assertEquals(expectedStatusCode, response.getStatusCode());
 
     }
 }
