6c9993b8da29257cffee587b4c9184a733ab61ce
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-09-05 12:15:01 +0100

    refactor: change stepdef methods to match feature file

--- PetStore3TestingFramework/src/test/java/stepdefs/FindPetByIdStepDefs.java
+++ PetStore3TestingFramework/src/test/java/stepdefs/FindPetByIdStepDefs.java
@@ -4,46 +4,45 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import models.FindPetByIdModel;
 
 public class FindPetByIdStepDefs {
-
-    private FindPetByIdModel findPetByIdModel;
-
     @Given("I have a valid pet ID")
     public void iHaveAValidPetID() {
-        findPetByIdModel = new FindPetByIdModel();
     }
 
     @When("I make a GET request to \\/pet\\/\\{petId}")
     public void iMakeAGETRequestToPetPetId() {
     }
 
-    @Then("I should receive a response with the pet's details")
-    public void iShouldReceiveAResponseWithThePetSDetails() {
+    @Then("I should receive a {int} OK response")
+    public void iShouldReceiveAOKResponse(int arg0) {
     }
 
-    @Given("I provide an invalid pet ID that does not exist")
-    public void iProvideAnInvalidPetIDThatDoesNotExist() {
+    @And("the pet details should be correct")
+    public void thePetDetailsShouldBeCorrect() {
     }
 
-    @Then("I should receive a {int} Not Found error")
-    public void iShouldReceiveANotFoundError(int errorCode) {
+    @Given("I have an invalid pet ID that does not exist")
+    public void iHaveAnInvalidPetIDThatDoesNotExist() {
     }
 
-    @And("the response message should indicate that the pet was not found")
-    public void theResponseMessageShouldIndicateThatThePetWasNotFound() {
+    @Then("I should receive a {int} Not Found response")
+    public void iShouldReceiveANotFoundResponse(int arg0) {
     }
 
-    @Given("I provide a pet ID that does not conform to the expected format")
-    public void iProvideAPetIDThatDoesNotConformToTheExpectedFormat() {
+    @And("an error message indicating that the pet was not found")
+    public void anErrorMessageIndicatingThatThePetWasNotFound() {
     }
 
-    @Then("I should receive a {int} Bad Request error")
-    public void iShouldReceiveABadRequestError(int errorCode) {
+    @Given("I have a pet ID in an invalid format")
+    public void iHaveAPetIDInAnInvalidFormat() {
     }
 
-    @And("the response message should indicate that the pet ID format is invalid")
-    public void theResponseMessageShouldIndicateThatThePetIDFormatIsInvalid() {
+    @Then("I should receive a {int} Bad Request response")
+    public void iShouldReceiveABadRequestResponse(int arg0) {
+    }
+
+    @And("an error message indicating that the pet ID format is invalid")
+    public void anErrorMessageIndicatingThatThePetIDFormatIsInvalid() {
     }
 }
