6f498dd3f5fb4c9bc24e51119e56277bae24d1f5
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-09-05 14:45:40 +0100

    feat: build steps to handle multiple pet ids and response validation

--- PetStore3TestingFramework/src/test/java/stepdefs/FindPetByIdStepDefs.java
+++ PetStore3TestingFramework/src/test/java/stepdefs/FindPetByIdStepDefs.java
@@ -1,73 +1,65 @@
 package stepdefs;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import models.FindPetByIdModel;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import xmltojava.Category;
-import xmltojava.FindPetByIdResponse;
-import xmltojava.TagsItem;
+import pojos.Category;
+import pojos.FindPetByIdResponse;
+import pojos.TagsItem;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindPetByIdStepDefs {
-    private Response response;
     private Integer petId;
-    private FindPetByIdResponse pet;
+    private FindPetByIdModel model;
+    private List<String> petIds;
+    private final Map<String, FindPetByIdResponse> responses = new HashMap<>();
+    private Map<String, Integer> responsesStatusCodes = new HashMap<>();
 
     @Given("I have a valid pet ID")
-    public void iHaveAValidPetID() {
-        petId = 3;
+    public void iHaveAValidPetID(DataTable dataTable) {
+        petIds = dataTable.asMaps(String.class, String.class).stream()
+                .map(map -> map.get("petId"))
+                .toList();
     }
 
     @When("I make get a request to find pet by id")
     public void iMakeAGETRequestToPetPetId() {
-        FindPetByIdModel findPetByIdModel = new FindPetByIdModel(petId);
-        findPetByIdModel.sendRequest();
+        for (String petId : petIds) {
+            model = new FindPetByIdModel(Integer.parseInt(petId));
+            model.sendRequest();
+            responses.put(petId, model.getResponse());
+            responsesStatusCodes.put(petId, model.getResponseStatusCode());
+        }
     }
 
     @Then("I should receive a 200 OK response")
     public void iShouldReceiveAOKResponse() {
-        MatcherAssert.assertThat(response.getStatusCode(), Matchers.is(200));
+        for (String petId : petIds) {
+            MatcherAssert.assertThat("Status code for pet ID " + petId, responsesStatusCodes.get(petId), Matchers.is(200));
+        }
     }
 
-    @And("the pet details should be:")
-    public void thePetDetailsShouldBe(DataTable dataTable) {
-        XmlMapper xmlMapper = new XmlMapper();
-        try {
-            pet = xmlMapper.readValue(response.getBody().asString(), FindPetByIdResponse.class);
-        } catch (Exception e) {
-            throw new AssertionError("Failed to deserialize XML response");
+    @And("the pet details should have an ID of")
+    public void thePetDetailsShouldHaveAnIDOf(DataTable dataTable) {
+        List<String> expectedPetIds = dataTable.asMaps(String.class, String.class).stream()
+                .map(map -> map.get("petId"))
+                .toList();
+
+        for (String expectedPetId : expectedPetIds) {
+            FindPetByIdResponse petResponse = responses.get(expectedPetId);
+            MatcherAssert.assertThat("Pet ID mismatch for expected ID " + expectedPetId, petResponse.id(), Matchers.is(Integer.parseInt(expectedPetId)));
         }
-
-        Map<String, String> data = dataTable.asMaps().getFirst();
-
-        assertEquals(Integer.parseInt(data.get("id")), pet.getId());
-        assertEquals(data.get("name"), pet.getName());
-        assertEquals(data.get("status"), pet.getStatus());
-
-        Category category = pet.getCategory();
-        assertEquals(Integer.parseInt(data.get("categoryId")), category.getId());
-        assertEquals(data.get("categoryName"), category.getName());
-
-        TagsItem tag = pet.getTags().getFirst();
-        assertEquals(Integer.parseInt(data.get("tagId")), tag.getId());
-        assertEquals(data.get("tagName"), tag.getName());
-
-        assertEquals(1, pet.getPhotoUrls().size());
-        assertEquals(data.get("photoUrl"), pet.getPhotoUrls().getFirst());
     }
 
     @Given("I have an invalid pet ID that does not exist")
